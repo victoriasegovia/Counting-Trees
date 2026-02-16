6bf48668f48011c0a9565e7137908b1ea4b79976
victoria <victoria.segovia.fz@gmail.com>
2026-02-16 11:47:16 +0100

    Leaflet implemented

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -1,5 +1,7 @@
 import TopBar from "../components/TopBar";
 import BottomBar from "../components/BottomBar";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "../CSS/AppLayout.css";
 
 export default function MapView() {
@@ -8,7 +10,19 @@ export default function MapView() {
             <TopBar />
 
             <div className="view">
-
+                <MapContainer
+                    center={[37.17684, -3.59587]} // coordinates
+                    zoom={13}
+                    style={{ width: "100%", height: "100%", borderRadius: "inherit" }}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[37.17684, -3.59587]}>
+                        <Popup>Ejemplo de ubicación</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
 
             <BottomBar />
