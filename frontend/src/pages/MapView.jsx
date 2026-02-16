697551a7d32e3f14c9c4efe48dcd0a33e73a6ad9
victoria <victoria.segovia.fz@gmail.com>
2026-02-16 11:59:24 +0100

    leaflet implemented and displayed in view

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -1,26 +1,37 @@
 import TopBar from "../components/TopBar";
 import BottomBar from "../components/BottomBar";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { DivIcon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "../CSS/AppLayout.css";
 
 export default function MapView() {
+
+    const plazaNueva = [37.17684, -3.59587];
+
+    const treeEmojiIcon = new DivIcon({
+        html: "🌳",
+        className: "emoji-marker",
+        iconSize: [50, 50],
+        iconAnchor: [25, 25],
+    })
+
     return (
         <>
             <TopBar />
 
             <div className="view">
                 <MapContainer
-                    center={[37.17684, -3.59587]} // coordinates
-                    zoom={13}
+                    center={plazaNueva} // coordinates
+                    zoom={20}
                     style={{ width: "100%", height: "100%", borderRadius: "inherit" }}
                 >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[37.17684, -3.59587]}>
-                        <Popup>Ejemplo de ubicación</Popup>
+                    <Marker position={plazaNueva} icon={treeEmojiIcon}>
+                        <Popup>Plaza Nueva, Granada</Popup>
                     </Marker>
                 </MapContainer>
             </div>
